d91af5dd9c827c16f9cc52fe9fd5a6759703b1bf
mahan <ataeifarmahan@gmail.com>
2025-11-28 12:29:09 +0330

    Inital commit with back

---
+++ back/Dong/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Dong project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dong.settings')
+
+application = get_asgi_application()
